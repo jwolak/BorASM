1095e2177b37aff7c5aa09097a078ebc08c9001c
Janusz <januszvdm@gmail.com>
2025-10-22 17:55:59 +0200

    Implement ICharacterStringLineHandler interface in CharacterStringLineHandler class

--- include/CharacterStringLineHandler.h
+++ include/CharacterStringLineHandler.h
@@ -4,14 +4,16 @@
 #include <string>
 #include <vector>
 
+#include "ICharacterStringLineHandler.h"
+
 namespace assembly_engine {
-    class CharacterStringLineHandler {
+    class CharacterStringLineHandler : public ICharacterStringLineHandler {
       public:
         CharacterStringLineHandler();
-        std::string CleanLineWhitespaces(const std::string& line_to_be_processed) const;
-        std::string RemoveLineComments(const std::string& line_to_be_processed) const;
-        bool IsNumber(const std::string& string_number) const;
-        uint8_t ConvertStringToNumber(const std::string& string_number) const;
-        std::vector<std::string> TokenizeLine(const std::string& line_to_be_processed) const;
+        std::string CleanLineWhitespaces(const std::string& line_to_be_processed) const override;
+        std::string RemoveLineComments(const std::string& line_to_be_processed) const override;
+        bool IsNumber(const std::string& string_number) const override;
+        uint8_t ConvertStringToNumber(const std::string& string_number) const override;
+        std::vector<std::string> TokenizeLine(const std::string& line_to_be_processed) const override;
     };
 }  // namespace assembly_engine

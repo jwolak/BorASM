4b5b4e2fcb238a002679ecee078a538a82ad4e88
Janusz <januszvdm@gmail.com>
2025-10-23 18:32:15 +0200

    Enhance AssemblyEngineTest with additional test cases for file assembly scenarios, including label detection, tokenization, and label resolution failures.

--- tests/src/AssemblyEngineTest.cpp
+++ tests/src/AssemblyEngineTest.cpp
@@ -6,9 +6,12 @@
 
 namespace assembly_engine_test {
 
+    using namespace testing;
+
     namespace {
         constexpr const char* kNonexistentInputFile = "nonexistent_input.asm";
         constexpr const char* kInvalidInputFile = "invalid_input.asm";
+        constexpr const char* kValidInputFile = "valid_input.asm";
         constexpr const char* kOutputFile = "output.bin";
     }  // namespace
 
@@ -25,17 +28,63 @@ namespace assembly_engine_test {
             : file_handler_mock{new mocks::FileHandlerMock()},
               code_analyzer_mock{new mocks::CodeAnalyzerMock()},
               assembly_engine_with_injected_mocks(std::unique_ptr<assembly_engine::IFileHandler>(file_handler_mock),
-                                                  std::unique_ptr<assembly_engine::ICodeAnalyzer>(code_analyzer_mock)) {}
+                                                  std::unique_ptr<assembly_engine::ICodeAnalyzer>(code_analyzer_mock)),
+              dummy_input_stream{} {}
 
         mocks::FileHandlerMock* file_handler_mock;
         mocks::CodeAnalyzerMock* code_analyzer_mock;
         AssemblyEngineWithInjectedMocks assembly_engine_with_injected_mocks;
+        std::ifstream dummy_input_stream;
     };
 
     TEST_F(AssemblyEngineTest, Try_Assemble_File_Nonexistent_Input_File_And_Returns_False) {
-        EXPECT_CALL(*file_handler_mock, OpenFileToRead(kNonexistentInputFile)).WillOnce(::testing::Return(false));
+        EXPECT_CALL(*file_handler_mock, OpenFileToRead(_)).WillOnce(::testing::Return(false));
+        EXPECT_CALL(*file_handler_mock, GetFileToReadStream()).Times(0);
+        EXPECT_CALL(*code_analyzer_mock, DetectLabels(_, _)).Times(0);
+        EXPECT_CALL(*code_analyzer_mock, Tokenize(_, _)).Times(0);
+        EXPECT_CALL(*code_analyzer_mock, ResolveLabelReferences()).Times(0);
 
         EXPECT_FALSE(assembly_engine_with_injected_mocks.Assemble(kNonexistentInputFile, kOutputFile));
+    }
+
+    TEST_F(AssemblyEngineTest, Try_Assemble_File_But_Detect_Lababels_Failed_And_Returns_False) {
+        EXPECT_CALL(*file_handler_mock, OpenFileToRead(_)).WillOnce(::testing::Return(true));
+        EXPECT_CALL(*file_handler_mock, GetFileToReadStream()).WillOnce(::testing::ReturnRef(dummy_input_stream));
+        EXPECT_CALL(*code_analyzer_mock, DetectLabels(_, _)).WillOnce(::testing::Return(false));
+        EXPECT_CALL(*code_analyzer_mock, Tokenize(_, _)).Times(0);
+        EXPECT_CALL(*code_analyzer_mock, ResolveLabelReferences()).Times(0);
+
+        EXPECT_FALSE(assembly_engine_with_injected_mocks.Assemble(kValidInputFile, kOutputFile));
+    }
+
+    TEST_F(AssemblyEngineTest, Try_Assemble_File_But_Tokenize_Failed_And_Returns_False) {
+        EXPECT_CALL(*file_handler_mock, OpenFileToRead(_)).WillOnce(::testing::Return(true));
+        EXPECT_CALL(*file_handler_mock, GetFileToReadStream()).WillOnce(::testing::ReturnRef(dummy_input_stream));
+        EXPECT_CALL(*code_analyzer_mock, DetectLabels(_, _)).WillOnce(::testing::Return(true));
+        EXPECT_CALL(*code_analyzer_mock, Tokenize(_, _)).WillOnce(::testing::Return(false));
+        EXPECT_CALL(*code_analyzer_mock, ResolveLabelReferences()).Times(0);
+
+        EXPECT_FALSE(assembly_engine_with_injected_mocks.Assemble(kValidInputFile, kOutputFile));
+    }
+
+    TEST_F(AssemblyEngineTest, Try_Assemble_File_But_Resolve_Labels_References_Failed_And_Returns_False) {
+        EXPECT_CALL(*file_handler_mock, OpenFileToRead(_)).WillOnce(::testing::Return(true));
+        EXPECT_CALL(*file_handler_mock, GetFileToReadStream()).WillOnce(::testing::ReturnRef(dummy_input_stream));
+        EXPECT_CALL(*code_analyzer_mock, DetectLabels(_, _)).WillOnce(::testing::Return(true));
+        EXPECT_CALL(*code_analyzer_mock, Tokenize(_, _)).WillOnce(::testing::Return(true));
+        EXPECT_CALL(*code_analyzer_mock, ResolveLabelReferences()).WillOnce(::testing::Return(false));
+
+        EXPECT_FALSE(assembly_engine_with_injected_mocks.Assemble(kValidInputFile, kOutputFile));
+    }
+
+    TEST_F(AssemblyEngineTest, Assemble_File_Successfully_And_Returns_True) {
+        EXPECT_CALL(*file_handler_mock, OpenFileToRead(_)).WillOnce(::testing::Return(true));
+        EXPECT_CALL(*file_handler_mock, GetFileToReadStream()).WillOnce(::testing::ReturnRef(dummy_input_stream));
+        EXPECT_CALL(*code_analyzer_mock, DetectLabels(_, _)).WillOnce(::testing::Return(true));
+        EXPECT_CALL(*code_analyzer_mock, Tokenize(_, _)).WillOnce(::testing::Return(true));
+        EXPECT_CALL(*code_analyzer_mock, ResolveLabelReferences()).WillOnce(::testing::Return(true));
+
+        EXPECT_TRUE(assembly_engine_with_injected_mocks.Assemble(kValidInputFile, kOutputFile));
     }
 
 }  // namespace assembly_engine_test
